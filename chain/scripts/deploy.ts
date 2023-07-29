53004820cff4f1af91f1c62bf5ce5773ef7e1bdf
Yash Thipsay <yashthpsay@gmail.com>
2023-07-29 11:43:15 +0530

    Fetching and writing address in a file(json data) to be used in react

--- chain/scripts/deploy.ts
+++ chain/scripts/deploy.ts
@@ -1,5 +1,8 @@
 import {ethers} from 'hardhat';
+import { MultiSig__factory } from '../typechain-types';
+import { Addressable } from 'ethers';
 const hre = require('hardhat');
+const fs = require('fs');
 
 async function getAddresses(){
   let addresses = [];
@@ -14,14 +17,23 @@ async function getAddresses(){
   
 }
 
+function createFile(address: string | Addressable){
+  type config = object;
+
+  const create: config = {address: address};
+  fs.writeFileSync("../webapp/__config.json", JSON.stringify(create, null, 2));
+}
+
 async function main(){
 let addresses = await getAddresses();
 console.log(addresses);
   const multisig= await ethers.getContractFactory("MultiSig");
-  const multi = await multisig.deploy(addresses,1);
-  
+  const multi = await multisig.deploy(addresses,3);
 
+  
   console.log('Contract deployed at', multi.target);
+
+   createFile(multi.target);
 }
 
 
