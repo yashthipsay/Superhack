788d26e3ce07539239307102f184cbb8c8b08419
Yash Thipsay <yashthpsay@gmail.com>
2023-07-29 11:04:17 +0530

    create function to fetch addresses from localhost

--- chain/scripts/deploy.ts
+++ chain/scripts/deploy.ts
@@ -1,11 +1,24 @@
 import {ethers} from 'hardhat';
 const hre = require('hardhat');
 
+async function getAddresses(){
+  let addresses = [];
+  const accounts = await ethers.getSigners()
+ 
+  
+  for (const account of accounts) {
+    addresses.push(account.address);
+  }
+
+  return addresses;
+  
+}
+
 async function main(){
-const accounts = await ethers.getSigners();
-console.log(accounts[0].address);
+let addresses = await getAddresses();
+console.log(addresses);
   const multisig= await ethers.getContractFactory("MultiSig");
-  const multi = await multisig.deploy([accounts[0].address, accounts[1].address, accounts[2].address],2);
+  const multi = await multisig.deploy(addresses,1);
   
 
   console.log('Contract deployed at', multi.target);
