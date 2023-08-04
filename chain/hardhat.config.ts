9a2f154d0d4c73a777dab029641a13edefb6d56e
Yash Thipsay <yashthpsay@gmail.com>
2023-08-04 15:45:37 +0530

    added optimism configuration(yet to deploy)

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -3,6 +3,26 @@ import "@nomicfoundation/hardhat-toolbox";
 
 require('dotenv').config();
 
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+try{
+const optimismGoerliUrl = process.env.ALCHEMY_API_KEY ? `https://opt-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}` : 
+  process.env.OPTIMISM_GOERLI_URL
+
+  const words = process.env.MNEMONIC.match(/[a-zA-Z]+/g).length;
+  // @ts-ignore
+  validLength = [12, 15, 18, 24]
+  if (!validLength.includes(words)) {
+     console.log(`The mnemonic (${process.env.MNEMONIC}) is the wrong number of words`)
+     process.exit(-1)
+  }
+}
+catch(error){
+  console.log("Error fetching info: " + error);
+}
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.17",
@@ -28,6 +48,10 @@ networks: {
     accounts: [process.env.WALLET_KEY as string],
     gasPrice: 1000000000,
   },
+  "optimism-goerli": {
+    url: optimismGoerliUrl,
+    accounts: { mnemonic: process.env.MNEMONIC }
+ }
 },
 defaultNetwork: 'hardhat',
 };
