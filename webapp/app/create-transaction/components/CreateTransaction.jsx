e0b08abf3edfb336c9e9e3f22488a029fae0c4ed
Soham <mastpanchalsoham@gmail.com>
2023-08-05 16:33:31 +0530

    feat/Added a brand logo strip on create-transaction page

---
+++ webapp/app/create-transaction/components/CreateTransaction.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './create_transaction.css'
+import BrandStrip from './BrandStrip'
+
+const CreateTransaction = () => {
+  return (
+    <div className='create-transaction-body'>
+        <BrandStrip />
+    </div>
+  )
+}
+
+export default CreateTransaction
