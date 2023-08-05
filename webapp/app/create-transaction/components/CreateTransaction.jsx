bb7f6df1fef67b66865436a421af38f891d9a8cb
Soham <mastpanchalsoham@gmail.com>
2023-08-05 19:35:02 +0530

    feat:Added strips design in the background of the create transaciton page

--- webapp/app/create-transaction/components/CreateTransaction.jsx
+++ webapp/app/create-transaction/components/CreateTransaction.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './create_transaction.css'
 import BrandStrip from './BrandStrip'
+
 import { Transaction } from 'ethers'
 
 import TransactionPanel from './TransactionPanel'
@@ -8,6 +9,8 @@ import TransactionPanel from './TransactionPanel'
 const CreateTransaction = () => {
   return (
     <div className='create-transaction-body'>
+        {/* <BackgroundStrips /> */}
+        <img className='strips-background' src="backgroud_designs/Union.png"  />
         <BrandStrip />
         <TransactionPanel />
     </div>
